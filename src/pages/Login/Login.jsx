211434f7cbf4936fae87898612e02d4e778a5def
unknown <tobiburrohman2@gmail.com>
2023-04-12 00:18:32 +0600

    redirect folder added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import PhoneInput from 'react-phone-number-input';
 import './Login.css';
-import '../Register/Register.css'
+// import '../Register/Register.css'
 import { Link, useNavigate } from 'react-router-dom';
 import Loader from '../../components/Loader/Loader';
 import Swal from 'sweetalert2';
